c8259a2e722f8ff51472fa6824eea0d0458cd6f5
Idax028 <s251236@dtu.dk>
2026-04-15 14:29:19 +0200

    Create opgave3.py

---
+++ opgave3.py
@@ -0,0 +1,3 @@
+#Denne model gør det nemt at tilgå og manipulere data, idet vi hurtigt kan finde ud af, 
+# hvilke sider der modtager links fra en bestemt side, hvilket er essentielt for PageRank algoritmen, da link-strukturen danner grundlaget 
+# for at bestemme sidernes relative vigtighed.
