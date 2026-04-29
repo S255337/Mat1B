faad56a91be0acd8fe17701254aef42ce878d8dc
S255337 <FabianHertzKofoed@gmail.com>
2026-04-29 13:39:58 +0200

    Update Opgave35.py

--- Opgave35.py
+++ Opgave35.py
@@ -1,30 +1,37 @@
 import numpy as np
+
+from Opgave5 import make_web
 from Opgave17 import modified_link_matrix
 
 def matrix_PageRank(web, power, d=0.85):
 
     ranking = dict()
-    
+
     pagelist = list(web.keys())
     N = len(pagelist)
-    
-    # Her laver vi den modificerede link matrix M
+
+    # Vi laver den modificerede link matrix (M)
     M = modified_link_matrix(web, pagelist, d)
-    
-    # Vi ganger M med sig selv power gange
+
+    # Vi beregner M^power, som opgaven vil have os til at gøre
     M_power = np.linalg.matrix_power(M, power)
-    
-    # Startvektor med lige sandsynlighed for alle sider
+
+    # Så starter vi med en startvektor, hvor alle sider har samme PageRank værdi (1/N)
     v = np.ones(N) / N
-    
-    # Vi ganger M_power med startvektoren v
-    v = M_power @ v
-    
-    # normaliser så summen bliver 1
-    v = v / np.sum(v)
-    
-    # lav dictionary som output
+
+    # Vi ganger vores M^power med startvektoren for at få vores endelige PageRank værdier
+    v = M_power.dot(v)
+
+    # Her normaliserer vi v, så summen af alle PageRank værdier bliver 1
+    v = v / sum(v)
+
+    # I for loopet her laver vi et dictionary for at matche hver side med dens PageRank værdi
     for i in range(N):
         ranking[pagelist[i]] = v[i]
-    
+
     return ranking
+
+web = {0: {1, 2}, 1: {2}, 2: {0}, 3: set()
+}
+ranking = matrix_PageRank(web, power=10)
+print(ranking)
