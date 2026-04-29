2353308cbbdd373a841df6ae8abe2eee939b8dfd
T-doggie-dogg <theodor.mgp99@gmail.com>
2026-04-29 16:54:58 +0200

    opg15 w opdatering

--- Opgave15.py
+++ Opgave15.py
@@ -48,9 +48,12 @@ def recursive_PageRank(web, stopvalue=0.0001, max_iterations=200, d=0.85):
             break
 
     return PageRanks, iteration
-
-W1 = {0: {1, 2}, 1: {2}, 2: {0}, 3: set()}
-W2 = {0: {1}, 1: {2}, 2: {3}, 3: {0}}
+#random netværk
+#W1 = {0: {1, 2}, 1: {2}, 2: {0}, 3: set()}
+#W2 = {0: {1}, 1: {2}, 2: {3}, 3: {0}}
+#Efter opg 1 og 2 W netværk.
+W1 = {0: {2, 4}, 1: {3, 4}, 2: {0}, 3: {0, 1, 4}, 4: set()}
+W2 = {0: {1}, 1: {2}, 2: {1}, 3: {0}, 4: {5}, 5: {6}, 6: {4}}
 
 print("W1:")
 ranks1, it1 = recursive_PageRank(W1)
